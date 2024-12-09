506bec1ce2accdc13a4b81012772d2c967a143f5
Aistis Draugelis <aistis.draugelis@ktu.edu>
2024-12-09 23:28:49 +0200

    Add user view and set urls

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+class UserListCreate(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
